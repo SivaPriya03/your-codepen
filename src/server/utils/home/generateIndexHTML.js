1c390b3cd7e4f1ed42151c7f82d53ce063a8348d
SivaPriya03 <sivapriya1700@gmail.com>
2021-08-21 21:53:13 +0530

    Home page added

--- src/server/utils/home/generateIndexHTML.js
+++ src/server/utils/home/generateIndexHTML.js
@@ -14,7 +14,21 @@ export default function generateIndexHTML(rootFolder){
         }
     });
     const globalVariableScript = `
-        <h1> Your CodePen </h1>
+        <header>
+            <h1 class="heading"> Your Work </h1>
+        </header>
+        <main class="app" id='root'>
+        </main>  
+        <footer>
+            <div class="footer">
+                <div class="name">
+                    Created by <a class='link' target="_blank" href='https://twitter.com/thepurplecoder'>thepurplecoder</a>
+                </div>
+                <div class="contribution">
+                    Find source code in <a class='link' target="_blank" href='https://github.com/SivaPriya03/your-codepen'>Github</a>
+                </div>
+            </div>
+        </footer>
         <script>
             var folders = ${folderStr.length > 0 } ? [${folderStr}] : [];
         </script>
--- src/server/utils/home/homepage.js
+++ src/server/utils/home/homepage.js
@@ -15,23 +15,40 @@ function Events(){
 }
 const globalClickEvent = new Events();
 document.addEventListener('click', globalClickEvent.listenToEvent);
-function SingleElement(elementName){
-    const name = elementName;
+function SingleElement(folderName){
+    const name = folderName;
     this.createElement = () => {
-        let element = document.createElement('div');
-        let iFrameElement = document.createElement('iframe');
-        let buttonEle = document.createElement('button');
-        buttonEle.textContent = 'Open Pen';
-        iFrameElement.src = `/${name}`;
-        function onClick(e){
-            if(e.target === buttonEle){
-                document.location.href = `/${name}`
-            }
-        }
-        globalClickEvent.addCallback(onClick);
-        element.appendChild(iFrameElement);
-        element.appendChild(buttonEle);
-        return element;
+        /*
+            <div class="iframeParent">
+                <iframe class="iframe" src='http://localhost:3000/newpen'></iframe>
+                <a class='iframeLink' href="/sample">
+                    
+                </a>
+                <h3>
+                    Sample
+                </h3>
+            </div> 
+        */
+       const iframeParent = document.createElement('div');
+       const iframeElement = document.createElement('iframe');
+       const iframeLink = document.createElement('a');
+       const heading = document.createElement('h3');
+       
+       iframeParent.className = 'iframeParent';
+       iframeElement.className = 'iframe';
+       iframeLink.className = 'iframeLink';
+       heading.className = 'pentitle';
+    
+       const src =  '/' + folderName;
+       iframeElement.src = src;
+       iframeLink.href = src;
+       heading.textContent = folderName; // Need to replace with a valid title
+       
+       iframeParent.appendChild(iframeElement);
+       iframeParent.appendChild(iframeLink);
+       iframeParent.appendChild(heading);
+       
+       return iframeParent;
     }
 }
 
@@ -41,8 +58,10 @@ function createElements(names){
         const element = new SingleElement(name);
         wholeParent.appendChild(element.createElement());
     });
+    wholeParent.className = 'codepen';
     return wholeParent;
 }
 
-const wholeParent = createElements(folders);
-document.body.appendChild(wholeParent);
+const iframeParent = createElements(folders);
+const rootEle = document.getElementById('root')
+rootEle.appendChild(iframeParent);
